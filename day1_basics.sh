873621e564fc58ccaf349fd984b88c24cc27596c
sortlight <149396850+sortlight@users.noreply.github.com>
2025-06-04 14:34:45 -0700

    Update day1_basics.sh

--- day1_basics.sh
+++ day1_basics.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-# Day 1: Basic Bash Commands
+#Sortsec Day 1: Basic Bash Commands
 echo "Welcome to Day 1 of the Bash Scripting Challenge!"
 mkdir test_folder
 cd test_folder
 touch hello.txt
 echo "This is a test file." > hello.txt
 cat hello.txt
+
+
