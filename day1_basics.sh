5cf73534835a07b320a18f45f4e3885572eea027
sortlight <149396850+sortlight@users.noreply.github.com>
2025-06-04 23:31:31 +0200

    Add files via upload

---
+++ day1_basics.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Day 1: Basic Bash Commands
+echo "Welcome to Day 1 of the Bash Scripting Challenge!"
+mkdir test_folder
+cd test_folder
+touch hello.txt
+echo "This is a test file." > hello.txt
+cat hello.txt
